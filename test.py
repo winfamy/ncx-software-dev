fbc09a81525a37e93d7e41e83ddc8967b9621828
ark-232 <arkady232@gmail.com>
2022-03-29 16:52:00 -0600

    enrique_branch

--- test.py
+++ test.py
@@ -3,9 +3,14 @@ import pytest
 from main import function_to_test
 
 # Sample
-def test_example():
-    assert(1 == 1)
-    assert(1 == 2)
+def test1():
+    assert(function_to_test(1,1) == 2)
+def test2():
+    assert(function_to_test(2,100) == 1)
+def test3():
+    assert(function_to_test(4,0) == 0)
+def test4():
+    assert(function_to_test(-1,420) == -1)
 
 # Test with
 # pytest -q test.py
