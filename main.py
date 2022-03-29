176a30639f47d8bd195dd597f8b9e3ae81ff6341
Grady Phillips <grady@unyx.io>
2022-03-29 16:29:57 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def function_to_test(a, b):
+    if a == 2:
+        return 1
+
+    if a > 2:
+        return 0
+    
+    if a == 1 and b == 1:
+        return 2
---
+++ test.py
@@ -0,0 +1,11 @@
+# Write unit tests here
+import pytest
+from main import function_to_test
+
+# Sample
+def test_example():
+    assert(1 == 1)
+    assert(1 == 2)
+
+# Test with
+# pytest -q test.py
