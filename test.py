2427b458fa17071fe897aa940e0e5024138fcdc0
D-Pretzel <petzoldavid02@gmail.com>
2022-03-29 16:51:08 -0600

    ha

--- test.py
+++ test.py
@@ -2,10 +2,14 @@
 import pytest
 from main import function_to_test
 
-# Sample
-def test_example():
-    assert(1 == 1)
-    assert(1 == 2)
+def test_one():
+    assert function_to_test(2, 0) == 1
+def test_two():
+    assert function_to_test(3, 69) == 0
+def test_three():
+    assert function_to_test(1, 1) == 2
+def test_four():
+    assert function_to_test(-1, 420) == -1
 
 # Test with
 # pytest -q test.py
