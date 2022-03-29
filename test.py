91c0caff3bc557c88af3f0d9436bc000f372da4a
Daniel Sevilla <c23daniel.sevilla@afacademy.af.edu>
2022-03-29 16:46:15 -0600

    Sevilla - assert PoC

--- test.py
+++ test.py
@@ -4,8 +4,10 @@ from main import function_to_test
 
 # Sample
 def test_example():
-    assert(1 == 1)
-    assert(1 == 2)
+    assert(function_to_test(1, 0) == -1)
+    assert(function_to_test(2, 0) == 1)
+    assert(function_to_test(3, 0) == 0)
+    assert(function_to_test(1, 1) == 2)
 
 # Test with
 # pytest -q test.py
